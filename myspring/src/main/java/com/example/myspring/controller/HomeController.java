e81a9864c1198c037a7d65ea80c84f901ef705b1
jslee <jslee@kb-sys.co.kr>
2021-10-25 12:07:05 +0900

    .

--- myspring/src/main/java/com/example/myspring/controller/HomeController.java
+++ myspring/src/main/java/com/example/myspring/controller/HomeController.java
@@ -1,20 +1,14 @@
-//package com.example.myspring.controller;
-//
-//import org.springframework.boot.SpringBootConfiguration;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@SpringBootConfiguration
-//@RestController
-//public class HomeController {
-//	@GetMapping("/")
-//	public String Hello() {
-//		return "Hello Fucking World!";
-//	}
-//	
-//	@RequestMapping(value = "/zzz")
-//	public String hi() {
-//		return "index";
-//	}
-//}
+package com.example.myspring.controller;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootConfiguration
+@RestController
+public class HomeController {
+	@GetMapping("/home")
+	public String Hello() {
+		return "Hello World!";
+	}
+}
